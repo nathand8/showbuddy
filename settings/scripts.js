40aad983aa879d5b7420c1b84e80bb156aa7cdf5
Jozomby <leechef2010@gmail.com>
2016-11-16 19:03:07 -0700

    Gather information from fields into user object

--- settings/scripts.js
+++ settings/scripts.js
@@ -1,4 +1,8 @@
 $(document).ready(function() {
+	let userInfo = {
+		event_preferences: []
+	}
+	let tagId = 0
 	$("#personalProfile").hide()
 	$("#eventPreferences").hide()
 	$("#buddyPreferences").hide()
@@ -13,8 +17,37 @@ $(document).ready(function() {
 	})
 	$("#addGenreButton").click(function() {
 		let contents = $("#addGenre").val()
-		let tag = "<div class='tag'>" + contents + "<span class='exit'>x</span></div>"
+		let curTag = tagId
+		let tag = "<div id='tag" + curTag + "' class='tag'>" + contents + "<span id='remove" + curTag + "' class='exit'>&times;</span></div>"
 		$("#genres").append(tag)
+		userInfo.event_preferences.push(contents)
 		$("#addGenre").val('')
+		$("#remove" + curTag).click(function() {
+			let index = userInfo.event_preferences.indexOf(contents)
+			userInfo.event_preferences.splice(index, 1)
+			console.log(userInfo)
+			$("#tag" + curTag).remove()
+		})
+		tagId++
+	})
+	$("#saveSettings").click(function() {
+		let file = document.querySelector('input[type=file]').files[0]
+		let reader = new FileReader()
+		reader.addEventListener("load", function() {
+			userInfo.picture = reader.result.split(",")[1]
+		}, false)
+		if (file) {
+			reader.readAsDataURL(file)
+		}
+		userInfo.screenname = $("#displayName").val()
+		userInfo.age = $("#age").val()
+		userInfo.gender = $("#gender").val()
+		userInfo.description = $("#bio").val()
+		userInfo.buddy_age_min = $("#minAge").val()
+		userInfo.buddy_age_max = $("#maxAge").val()
+		userInfo.buddy_gender = $("#buddygender").val()
+		userInfo.individual = $("#individuals").is(':checked')
+		userInfo.group = $("#groups").is(':checked')
+		console.log(userInfo)
 	})
 })
