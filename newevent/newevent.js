65269e2f592874be76807357616bf7a1e83b9fe0
Trevor Anderson <trevor.anderson16@gmail.com>
2016-11-20 14:45:58 -0700

    Formatting for the create event page finished.

--- newevent/newevent.js
+++ newevent/newevent.js
@@ -1,16 +1,36 @@
 $(document).ready(function() {
-    $("#date").datepicker();
+	$("#date").datepicker();
 	$('input.timepicker').timepicker({
 		timeFormat: 'h:mm p',
-    interval: 30,
-    minTime: '0',
-    maxTime: '11:30pm',
-    defaultTime: '18',
-    startTime: '0:00',
-    dynamic: false,
-    dropdown: true,
-    scrollbar: true
-		
+		interval: 30,
+		minTime: '0',
+		maxTime: '11:30pm',
+		defaultTime: '18',
+		startTime: '0:00',
+		dynamic: false,
+		dropdown: true,
+		scrollbar: true
 	});
-    });
+});
 
+function addArtist(){
+        var li = document.createElement("li");  
+        var input = document.getElementById("artists");
+        li.innerHTML = input.value;
+        input.value = "";
+
+        document.getElementById("artistlist").appendChild(li);
+    }
+	
+	function addGenre(){
+        var li = document.createElement("li");  
+        var input = document.getElementById("genres");
+        li.innerHTML = input.value;
+        input.value = "";
+
+        document.getElementById("genrelist").appendChild(li);
+    }
+	
+	function submitForm() {
+		var 
+	}
