67c181226c00dd0b5aec22d00a23ca117ac77138
Trevor Anderson <trevor.anderson16@gmail.com>
2016-11-20 18:20:19 -0700

    New Event database calls

--- newevent/newevent.js
+++ newevent/newevent.js
@@ -10,7 +10,11 @@ $(document).ready(function() {
 		dynamic: false,
 		dropdown: true,
 		scrollbar: true
-	});
+	})
+	$('#submitbutton').click(function() {
+		submitForm();
+	})
+	dao_setup();
 });
 
 function addArtist(){
@@ -20,17 +24,43 @@ function addArtist(){
         input.value = "";
 
         document.getElementById("artistlist").appendChild(li);
-    }
+};
 	
-	function addGenre(){
+function addGenre(){
         var li = document.createElement("li");  
         var input = document.getElementById("genres");
         li.innerHTML = input.value;
         input.value = "";
 
         document.getElementById("genrelist").appendChild(li);
-    }
+};
+
+function submit() {
+	var eventobj = submitForm();
+	var title = $('#title').value;
+	dao_setEventByID(title, eventobj);
+};
 	
-	function submitForm() {
-		var 
-	}
+function submitForm() {
+		var eventObj = {};
+		var title = document.getElementById("title").value;
+		eventObj["title"] = title;
+		var artistlist = document.getElementById("artistlist");
+		var artists = {};
+		for (var i = 0; i < artistlist.length; i++) {
+			artists[i] = artistlist.childNodes[0].childNodes[i].nodeValue;
+		};
+		eventObj["artists"] = JSON.stringify(artists);
+		eventObj["date"] = document.getElementById("date").value;
+		eventObj["time"] = document.getElementById("time").value;
+		eventObj["venue"] = document.getElementById("venue").value;
+		var genrelist = document.getElementById("genrelist");
+		var genres = {};
+		for (var i = 0; i < genrelist.length; i++) {
+			genres[i] = genrelist.childNodes[0].childNodes[i].nodeValue;
+		};
+		eventObj["genres"] = JSON.stringify(genres);
+		eventObj["description"] = document.getElementById("desc").value;
+		
+		return JSON.stringify(eventObj);
+};
