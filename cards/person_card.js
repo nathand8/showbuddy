460efb9f634b6e9dc161dd34ad5c802691845b82
Ellery Davila <ellerydavila@gmail.com>
2016-11-20 19:36:58 -0700

    tinder stuff

---
+++ cards/person_card.js
@@ -0,0 +1,33 @@
+function setText(containerDiv, selectorId, text) {
+    if (containerDiv && selectorId && text)
+        containerDiv.find('[id="' + selectorId + '"]')[0].innerHTML = '<span>' + text + '</span>';
+}
+
+function loadPersonCard(cardDiv, p) {
+    if (!cardDiv || !p) {
+        console.error("Missing card div or event");
+        return false;
+    }
+
+    setText(cardDiv, "person_name", p.name);
+    setText(cardDiv, "person_age", p.age);
+    setText(cardDiv, "person_description", p.description);
+
+    if (p.picture) {
+        cardDiv.find('[id="person_picture"]')[0].innerHTML = '<img class="person_picture" src="' + p.picture + '">';
+    }
+}
+
+function rejectPerson(actionElem) {
+    $(actionElem).parent().addClass("rejected");
+    setTimeout(function() {
+        $(actionElem).parent().remove();
+    }, 500);
+}
+
+function acceptPerson(actionElem) {
+    $(actionElem).parent().addClass("accepted");
+    setTimeout(function() {
+        $(actionElem).parent().remove();
+    }, 500);
+}
