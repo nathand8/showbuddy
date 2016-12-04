1e26fdac6bf861b30650403908b1976c196759e3
Ellery Davila <ellerydavila@gmail.com>
2016-12-03 18:02:01 -0700

    messages to matches

--- messages/cards/convo_card.js
+++ messages/cards/convo_card.js
@@ -6,38 +6,18 @@ function setText(containerDiv, selectorId, text) {
 function loadConvoCard(cardDiv, c) {
 
     if (!cardDiv || !c) {
-        console.error("Missing card div or convo");
+        console.error("Missing card div or match");
         return false;
     }
 
     cardDiv.find('[id="convo_card"]')[0].id = c.convoId;
 
-    var person_name = c.convoId.split('_')[1];
+    setText(cardDiv, "person_name", c.name);
 
-    setText(cardDiv, "person_name", person_name);
+    setText(cardDiv, "person_phone", c.phone);
+    setText(cardDiv, "person_email", c.email);
 
-    var time = c.messages[c.messages.length - 1].timestamp;
-
-    var dateString = (time.getMonth() + 1)
-        + '/' + time.getDate()
-        + '/' + (time.getFullYear().toString().slice(-2));
-
-    var hours = time.getHours();
-    var timeSuffix = "am";
-    if (hours > 12) {
-        hours = hours - 12;
-        timeSuffix = "pm";
-    }
-    var minutes = time.getMinutes().toString();
-    if (minutes.length === 1) {
-        minutes = '0' + minutes;
-    }
-    var timeString = (hours + ":" + minutes  + " " + timeSuffix);
-    setText(cardDiv, "convo_time", dateString + ', ' + timeString);
-
-    setText(cardDiv, "convo_message", c.messages[c.messages.length - 1].message);
-
-    cardDiv.find('[id="person_picture"]')[0].innerHTML = '<img class="person-picture" src="/images/peoplePlaceholders/' + person_name + '.jpg">';
+    cardDiv.find('[id="person_picture"]')[0].innerHTML = '<img class="person-picture" src="/images/peoplePlaceholders/' + c.name + '.jpg">';
 }
 
 function loadMsgCards(cId) {
--- messages/script.js
+++ messages/script.js
@@ -2,41 +2,21 @@ $(function(){
     loadCards();
 });
 
-var test_convos = [
+var test_matches = [
     {
-        convoId: "testUser_harambe",
-        messages: [
-            {
-                message: "Let's carpool to the concert this weekend",
-                sender: "testUser",
-                timestamp: new Date(2016,10,21,11,30)
-            },
-            {
-                message: "That's a good idea. I'll pick you up at 6",
-                sender: "harambe",
-                timestamp: new Date(2016,10,21,11,45)
-            }
-        ]
+        name: "harambe",
+        phone: "555-333-1234",
+        email: "harambe@zoo.org"
     },
     {
-        convoId: "testUser_trump",
-        messages: [
-            {
-                message: "You have to come to my concert. I have the best concerts. It's gonna be yuuge!",
-                sender: "trump",
-                timestamp: new Date(2016,10,20,11,30)
-            },
-            {
-                message: "I'll be there :)",
-                sender: "testUser",
-                timestamp: new Date(2016,10,20,11,45)
-            }
-        ]
+        name: "trump",
+        phone: "555-111-4444",
+        email: "trump@usa.gov"
     }
 ];
 
 function loadCards() {
-    $.each(test_convos, function(i, c) {
+    $.each(test_matches, function(i, c) {
         var convoDiv = $(document.createElement('DIV'));
         $('div.convo-cards').append(convoDiv);
         convoDiv.load("/messages/cards/convo_card.html", function() {
