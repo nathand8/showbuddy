34e5e88b633689e84a573b9aef944a2e50043613
Ellery Davila <ellerydavila@gmail.com>
2016-12-06 17:25:09 -0700

    simplified event card's date/time for FireBase hard-coding

--- cards/event_card.js
+++ cards/event_card.js
@@ -15,25 +15,10 @@ function loadEventCard(cardDiv, ev, showButtons) {
     }
 
     setText(cardDiv, "event_title", ev.title);
-    if (ev.datetime) {
-        var dateString = (ev.datetime.getMonth() + 1) 
-            + '/' + ev.datetime.getDate() 
-            + '/' + (ev.datetime.getFullYear().toString().slice(-2));
-        setText(cardDiv, "event_date", dateString);
 
-        var hours = ev.datetime.getHours();
-        var timeSuffix = "am";
-        if (hours > 12) {
-            hours = hours - 12;
-            timeSuffix = "pm";
-        }
-        var minutes = ev.datetime.getMinutes().toString();
-        if (minutes.length === 1) {
-            minutes = '0' + minutes;
-        }
-        var timeString = (hours + ":" + minutes  + " " + timeSuffix);
-        setText(cardDiv, "event_time", timeString);
-    }
+    setText(cardDiv, "event_date", ev.date);
+    setText(cardDiv, "event_time", ev.time);
+
     setText(cardDiv, "event_venue", ev.venue);
     setText(cardDiv, "event_description", ev.description);
 
